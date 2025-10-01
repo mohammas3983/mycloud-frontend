b26da77352562d3c64391a34eccd9622a8a9c88d
MohammadSadegh <95463454+mohammas3983@users.noreply.github.com>
2025-10-01 15:32:57 +0330

    Prepare for Vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,6 @@ const App = () => {
         <Sonner />
         
         <AuthProvider>
-          <BrowserRouter basename="/mycloud-frontend/">
             <Routes>
               {/* مسیرهای عمومی */}
               <Route path="/login" element={<Login />} />
